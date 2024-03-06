f2b7d128500a1f20c32a4d08a797761da4661f7f
B7AST <85998893+B7AST@users.noreply.github.com>
2024-03-06 12:58:42 +0200

    Create Project and Header

---
+++ date_range_picker/src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/header/Header.jsx";
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
